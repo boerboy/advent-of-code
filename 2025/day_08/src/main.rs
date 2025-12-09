7a664da7f1a57f6c60c08ca47e9b3cac9d2d6588
Smith <andrew.smith@swissborg.com>
2025-12-09 12:44:44 +0200

    Day 8

--- 2025/day_08/src/main.rs
+++ 2025/day_08/src/main.rs
@@ -3,9 +3,9 @@
 //! Puzzle: https://adventofcode.com/2025/day/8
 
 use anyhow::Result;
-use common::grid::VisitedTracker;
-use common::input::{read_csv, read_grid};
+use common::input::read_csv;
 use common::prelude::Itertools;
+use std::collections::HashMap;
 
 /// Get path relative to this crate's directory
 fn resource(filename: &str) -> String {
@@ -19,7 +19,55 @@ fn test_input() -> String {
     resource("test.txt")
 }
 
-fn distance(point1: &Vec<i64>, point2: &Vec<i64>) -> f64 {
+/// Union-Find data structure for tracking connected components
+struct UnionFind {
+    parent: Vec<usize>,
+    num_circuits: usize,
+}
+
+impl UnionFind {
+    fn new(size: usize) -> Self {
+        Self {
+            parent: (0..size).collect(),
+            num_circuits: size,
+        }
+    }
+
+    fn find(&mut self, idx: usize) -> usize {
+        if self.parent[idx] != idx {
+            self.parent[idx] = self.find(self.parent[idx]);
+        }
+        self.parent[idx]
+    }
+
+    /// Union two elements, returns true if they were in different circuits
+    fn union(&mut self, i: usize, j: usize) -> bool {
+        let (root_i, root_j) = (self.find(i), self.find(j));
+        if root_i != root_j {
+            self.parent[root_i] = root_j;
+            self.num_circuits -= 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn circuits(&mut self) -> Vec<Vec<usize>> {
+        let size = self.parent.len();
+        (0..size)
+            .fold(HashMap::new(), |mut map, idx| {
+                map.entry(self.find(idx))
+                    .or_insert_with(Vec::new)
+                    .push(idx);
+                map
+            })
+            .into_values()
+            .collect()
+    }
+}
+
+/// Calculate Euclidean distance between two 3D points
+fn distance(point1: &[i64], point2: &[i64]) -> f64 {
     point1
         .iter()
         .zip(point2)
@@ -31,7 +79,8 @@ fn distance(point1: &Vec<i64>, point2: &Vec<i64>) -> f64 {
         .sqrt()
 }
 
-fn distances(points: &Vec<Vec<i64>>) -> Vec<(usize, usize, f64)> {
+/// Calculate distances between all pairs of points
+fn distances(points: &[Vec<i64>]) -> Vec<(usize, usize, f64)> {
     points
         .iter()
         .enumerate()
@@ -44,29 +93,58 @@ fn distances(points: &Vec<Vec<i64>>) -> Vec<(usize, usize, f64)> {
         .collect()
 }
 
-fn create_circuits(distances: Vec<(usize, usize, f64)>) -> Vec<i64>{
- ???
+/// Sort distances by length (ascending)
+fn sorted_distances(points: &[Vec<i64>]) -> Vec<(usize, usize, f64)> {
+    distances(points)
+        .into_iter()
+        .sorted_by(|a, b| a.2.partial_cmp(&b.2).unwrap())
+        .collect()
 }
 
 fn part1(path: &str, n: usize) -> Result<i64> {
     let input: Vec<Vec<i64>> =
         read_csv(path, b',').map_err(|err| anyhow::anyhow!(err.to_string()))?;
-    let distances = distances(&input);
-    let circuits = create_circuits(distances);
-    let result = circuits.iter().sorted_by(|a, b| b.cmp(&a)).take(n).sum();
+
+    let shortest = sorted_distances(&input).into_iter().take(n);
+
+    let mut uf = UnionFind::new(input.len());
+    shortest.for_each(|(i, j, _)| {
+        uf.union(i, j);
+    });
+
+    let result = uf
+        .circuits()
+        .iter()
+        .map(|x| x.len() as i64)
+        .sorted_by(|a, b| b.cmp(a))
+        .take(3)
+        .product();
+
     Ok(result)
 }
 
 fn part2(path: &str) -> Result<i64> {
-    let input = read_grid(path)?;
-    Ok(0)
+    let input: Vec<Vec<i64>> =
+        read_csv(path, b',').map_err(|err| anyhow::anyhow!(err.to_string()))?;
+
+    let all_distances = sorted_distances(&input);
+
+    let mut uf = UnionFind::new(input.len());
+    let (idx1, idx2) = all_distances
+        .iter()
+        .find(|&&(i, j, _)| uf.union(i, j) && uf.num_circuits == 1)
+        .map(|&(i, j, _)| (i, j))
+        .ok_or_else(|| anyhow::anyhow!("No final connection found"))?;
+
+    Ok(input[idx1][0] * input[idx2][0])
 }
 
 fn main() -> Result<()> {
     println!("=== Advent of Code 2025 - Day 08 ===\n");
+    // Fell a bit behind on time so used a decent amount of LLM to get to solution
     // Run with test input first
     println!("--- Test Input ---");
-    match part1(&test_input(), 3) {
+    match part1(&test_input(), 10) {
         Ok(result) => println!("Part 1 (test): {}", result),
         Err(e) => println!("Part 1 (test) error: {}", e),
     }
@@ -74,12 +152,12 @@ fn main() -> Result<()> {
         Ok(result) => println!("Part 2 (test): {}", result),
         Err(e) => println!("Part 2 (test) error: {}", e),
     }
-    //
-    // println!("\n--- Puzzle Input ---");
-    // match part1(&input(), 1000) {
-    //     Ok(result) => println!("Part 1: {}", result),
-    //     Err(e) => println!("Part 1 error: {}", e),
-    // }
+    // Run with input
+    println!("\n--- Puzzle Input ---");
+    match part1(&input(), 1000) {
+        Ok(result) => println!("Part 1: {}", result),
+        Err(e) => println!("Part 1 error: {}", e),
+    }
     match part2(&input()) {
         Ok(result) => println!("Part 2: {}", result),
         Err(e) => println!("Part 2 error: {}", e),
